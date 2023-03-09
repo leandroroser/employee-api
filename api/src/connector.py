9fc3fd1d3ff8d4c349224993840fab201889c695
leandroroser <learoser@gmail.com>
2023-03-09 05:29:16 -0300

    fix

--- api/src/connector.py
+++ api/src/connector.py
@@ -7,38 +7,49 @@ from fastavro import reader, parse_schema
 from io import BytesIO
 from minio import Minio
 from datetime import date
+from sqlalchemy.orm import declarative_base
 
 
-class BaseMapper(ABC):
-    @abstractmethod
-    def from_domain(self, obj: any) -> BaseSchema:
-        pass
+Base = declarative_base()
 
-    @abstractmethod
-    def to_domain(self, obj: BaseSchema) -> any:
-        pass
+class BaseSchema(BaseModel):
+    class Config:
+        orm_mode = True
+
+    @property
+    def dict_for_model(self) -> PayloadDict:
+        return self.dict(by_alias=False)
 
 
-class Connector:
-    def __init__(self, db_session: Session, mapper: BaseMapper, model: Type[Base]):
+BaseT = TypeVar("BaseT", bound=Base)
+BaseSchemaT = TypeVar("BaseOrmModelT", bound=BaseSchema)
+
+class Connector(Generic[BaseT, BaseSchemaT]):
+    def __init__(self, db_session: Session, entity: Base, domain:BaseSchema):
         self.db_session = db_session
-        self.mapper = mapper
-        self.model = model
+        self.entity = entity
+        self.domain = domain
 
-    def write(self, obj: BaseSchema):
+    def to_domain(self, obj: BaseSchema) -> Type[BaseT]:
+        return self.entity(**obj.dict())
+
+    def from_domain(self, obj: Base) -> Type[BaseSchemaT]:
+        return BaseSchema.from_orm(obj)
+
+    def write(self, obj: BaseSchemaT):
         db_obj = self.mapper.to_domain(obj)
         self.db_session.add(db_obj)
         self.db_session.commit()
 
-    def read_all(self) -> List[BaseSchema]:
+    def read_all(self) -> List[BaseSchemaT]:
         db_objs = self.db_session.query(self.model).all()
         return [self.mapper.from_domain(db_obj) for db_obj in db_objs]
 
-    def read_by_id(self, id: int) -> BaseSchema:
+    def read_by_id(self, id: int) -> BaseSchemaT:
         db_obj = self.db_session.query(self.model).filter_by(id=id).first()
         return self.mapper.from_domain(db_obj) if db_obj else None
 
-    def update(self, obj: BaseSchema):
+    def update(self, obj: BaseSchemaT):
         db_obj = self.db_session.query(self.model).filter_by(id=obj.id).first()
         if db_obj:
             for key, value in obj.dict(exclude_unset=True).items():
